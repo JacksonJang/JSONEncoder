1b0bba79aab37b234ad7e9018644717967279e17
장효원 <hyo961015@naver.com>
2021-12-22 22:21:40 +0900

    Create json string through JSONEncoder generally

---
+++ JSONEncoder/Model.swift
@@ -0,0 +1,13 @@
+//
+//  Model.swift
+//  JSONEncoder
+//
+//  Created by 장효원 on 2021/12/22.
+//
+
+import UIKit
+
+struct Employee: Codable {
+    var name: String
+    var email: String
+}
--- JSONEncoder/ViewController.swift
+++ JSONEncoder/ViewController.swift
@@ -11,9 +11,23 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let jsonCoder = JSONEncoder()
+        
+        do {
+            let data = try jsonCoder.encode(createEmployeeModel())
+            
+            guard let logString = String(data: data, encoding: .utf8) else { return }
+            
+            print("JSON Example : ", logString)
+        } catch {
+            
+        }
+        
     }
 
-
+    func createEmployeeModel() -> Employee {
+        return Employee(name: "Jackson Jang", email: "example@example.com")
+    }
 }
 
